bb082d7aff2d2b0e68402b21a7236504afbf9bda
nehadevaraj <ec22665@qmul.ac.uk>
2023-10-05 09:54:24 +0100

    code_works

---
+++ code.py
@@ -0,0 +1,21 @@
+import unittest
+
+def add_numbers(a,b):
+    return a + b
+
+class TestFunction(unittest.TestCase):
+
+    def test_addition(self):
+        result = add_numbers(3,5)
+        self.assertEqual(result, 8)
+
+    def test_addition_negative_numbers(self):
+        result = add_numbers(-2, 7)
+        self.assertEqual(result, 5)
+
+    def test_addition_float_numbers(self):
+        result = add_numbers(1.5, 2.5)
+        self.assertAlmostEqual(result, 4.0, places=2)
+
+if __name__ == '__main__':
+    unittest.main()
