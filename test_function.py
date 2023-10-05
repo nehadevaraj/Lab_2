606dc6e0feeecea91f21c69917bf932045d4a638
nehadevaraj <ec22665@qmul.ac.uk>
2023-10-05 10:07:24 +0100

    activity_2

---
+++ test_function.py
@@ -0,0 +1,8 @@
+from code import add_numbers
+
+real_num_1 = add_numbers(13, 3)
+print (real_num_1)
+real_num_2 = add_numbers(-13, 3)
+print (real_num_2)
+real_num_3 =add_numbers(13.3, 3.0)
+print (real_num_3)
